fde366e230cfc4e10261cef23225e8408f9b0027
zypang <pangzaiyu@163.com>
2020-08-06 15:36:55 +0800

    test gui client pass

---
+++ gui_client.py
@@ -0,0 +1,120 @@
+import sys
+
+from PyQt5.QtCore import QUrl, QDir
+from PyQt5.QtGui import QFontDatabase,QFont
+from PyQt5.QtWebEngineWidgets import QWebEngineSettings, QWebEnginePage
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtWebEngineWidgets
+from PyQt5 import QtCore
+
+from mymdict import MDD,MDX
+
+print("Loading dictionaries from file ...")
+mdx = MDX("/mnt/document/Dictionary/L6mp3.mdx")
+word_map={}
+for index,key_tuple in enumerate(mdx._key_list):
+    word_map[key_tuple[1].decode("utf-8")]=index
+print("Done")
+
+def lookup(word):
+    #mdx=MDX("LongmanDict.mdx")
+    index=word_map[word]
+    word,record = mdx._decode_record_by_key_start(index)
+    return word.decode("UTF-8"), record.decode("UTF-8")
+    #return mdx.items().__next__()[0]
+    #for key,value in mdx.items():
+    #    key=key.decode('UTF-8')
+    #    if key == word:
+    #        return value
+def search():
+    print("********** Search *****************")
+    while True:
+        word = input("word:")
+        w, r = test(word)
+        print(w)
+        print(r)
+def output(word):
+    _,record=test(word)
+    print(record)
+
+def loadJS(view,SCRIPT,name):
+    script = QtWebEngineWidgets.QWebEngineScript()
+    view.page().runJavaScript(SCRIPT, QtWebEngineWidgets.QWebEngineScript.ApplicationWorld)
+    script.setName(name)
+    script.setSourceCode(SCRIPT)
+    script.setInjectionPoint(QtWebEngineWidgets.QWebEngineScript.DocumentReady)
+    script.setRunsOnSubFrames(True)
+    script.setWorldId(QtWebEngineWidgets.QWebEngineScript.MainWorld)
+    view.page().scripts().insert(script)
+
+def load_css_js_from_file(type,view,path,name):
+    path = QtCore.QFile(path)
+    if not path.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text):
+        return
+    content = path.readAll().data().decode("utf-8")
+    if type=='css':
+        loadCSS(view,content,name)
+    else:
+        loadJS(view,content,name)
+
+def loadCSS(view, css, name):
+    SCRIPT = """
+    (function() {
+    css = document.createElement('style');
+    css.type = 'text/css';
+    css.id = "%s";
+    document.head.appendChild(css);
+    css.innerText = `%s`;
+    })()
+    """ % (name, css)
+
+    loadJS(view,SCRIPT,name)
+
+
+def set_default_font():
+    fontDataBase = QFontDatabase()
+    defaultSettings = QWebEngineSettings.globalSettings()
+    standardFont = fontDataBase.font("Noto Sans CJK SC", "", 12)
+    defaultSettings.setFontFamily(QWebEngineSettings.StandardFont, standardFont.family())
+    defaultSettings.setFontSize(QWebEngineSettings.DefaultFontSize, 18)
+
+class MyWebPage(QWebEnginePage):
+    def acceptNavigationRequest(self, url: QUrl, type: QWebEnginePage.NavigationType, isMainFrame: bool, **kwargs):
+        print(type)
+        if type == QWebEnginePage.NavigationTypeLinkClicked:
+            print(url)
+            scheme,item = url.toString().split("://")
+            _,record=lookup(item)
+            print(self.url())
+            self.setHtml(record,self.url())
+            return False
+        return True
+
+
+
+
+def test_qt():
+    #header='<meta charset="UTF-8"/>\n'
+    _,record=lookup("mouse")
+    #print(QDir.currentPath())
+    #print(record)
+    #record=header+record
+    app=QApplication([])
+    set_default_font()
+    view = QtWebEngineWidgets.QWebEngineView()
+    page=MyWebPage()
+    view.setPage(page)
+    baseUrl = QUrl.fromLocalFile(QDir.currentPath() + "/index.html")
+    print(baseUrl)
+    page.setHtml(record,baseUrl)
+    #view.load(QUrl("file://t.html"))
+    #load_css_js_from_file('css',view,"LDOCE6.css","ld6")
+    #load_css_js_from_file('js',view,"entry.js","entry")
+    view.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    #output(300)
+    test_qt()
+
+
--- mymdict.py
+++ mymdict.py
@@ -99,6 +99,7 @@ class MDict(object):
         self.header = self._read_header()
         try:
             self._key_list = self._read_keys()
+            self._record_block_info_list = self._decode_record_block_info()
             #print(self._key_list)
         except:
             print("Try Brutal Force on Encrypted Key Blocks")
@@ -404,6 +405,30 @@ class MDict(object):
         self._num_entries = len(key_list)
         return key_list
 
+    def _decode_record_block_info(self):
+        f = open(self._fname, 'rb')
+        f.seek(self._record_block_offset)
+
+        num_record_blocks = self._read_number(f)
+        num_entries = self._read_number(f)
+        assert (num_entries == self._num_entries)
+        record_block_info_size = self._read_number(f)
+        record_block_size = self._read_number(f)
+
+        # record block info section
+        record_block_info_list = []
+        size_counter = 0
+        for i in range(num_record_blocks):
+            compressed_size = self._read_number(f)
+            decompressed_size = self._read_number(f)
+            record_block_info_list += [(compressed_size, decompressed_size)]
+            size_counter += self._number_width * 2
+
+        f.close()
+        assert (size_counter == record_block_info_size)
+        return record_block_info_list
+
+
 
 class MDD(MDict):
     """
@@ -478,7 +503,7 @@ class MDD(MDict):
                     break
                 # record end index
                 if i < len(self._key_list)-1:
-                    record_end = self._key_list[i+1][0]
+                    recorreadmdictd_end = self._key_list[i+1][0]
                 else:
                     record_end = len(record_block) + offset
                 i += 1
@@ -522,31 +547,76 @@ class MDX(MDict):
                 txt_styled = txt_styled + style[0] + p + style[1]
         return txt_styled
 
-    def _decode_record_block(self):
+    def _decode_record_by_key_start(self,key_index):
+        key_start=self._key_list[key_index][0]
+        block_uncompressed_offset=0
+        block_compressed_offset=0
+
+        compressed_size = 0
+        decompressed_size = 0
+        for _compressed_size,_decompressed_size in self._record_block_info_list:
+            if block_uncompressed_offset+_decompressed_size > key_start:
+                compressed_size=_compressed_size
+                decompressed_size=_decompressed_size
+                break
+            block_uncompressed_offset+=_decompressed_size
+            block_compressed_offset+=_compressed_size
+
         f = open(self._fname, 'rb')
-        f.seek(self._record_block_offset)
+        f.seek(self._record_block_offset
+               + self._number_width * 4
+               + self._number_width * 2 * len(self._record_block_info_list)
+               + block_compressed_offset)
 
-        num_record_blocks = self._read_number(f)
-        num_entries = self._read_number(f)
-        assert(num_entries == self._num_entries)
-        record_block_info_size = self._read_number(f)
-        record_block_size = self._read_number(f)
+        record_block_compressed = f.read(compressed_size)
+        # 4 bytes indicates block compression type
+        record_block_type = record_block_compressed[:4]
+        # 4 bytes adler checksum of uncompressed content
+        adler32 = unpack('>I', record_block_compressed[4:8])[0]
+        # no compression
+        if record_block_type == b'\x00\x00\x00\x00':
+            record_block = record_block_compressed[8:]
+        # lzo compression
+        elif record_block_type == b'\x01\x00\x00\x00':
+            if lzo is None:
+                print("LZO compression is not supported")
+                raise Exception("LZO not supported")
+            # decompress
+            header = b'\xf0' + pack('>I', decompressed_size)
+            record_block = lzo.decompress(header + record_block_compressed[8:])
+        # zlib compression
+        elif record_block_type == b'\x02\x00\x00\x00':
+            # decompress
+            record_block = zlib.decompress(record_block_compressed[8:])
 
-        # record block info section
-        record_block_info_list = []
-        size_counter = 0
-        for i in range(num_record_blocks):
-            compressed_size = self._read_number(f)
-            decompressed_size = self._read_number(f)
-            record_block_info_list += [(compressed_size, decompressed_size)]
-            size_counter += self._number_width * 2
-        assert(size_counter == record_block_info_size)
+        # notice that adler32 return signed value
+        assert (adler32 == zlib.adler32(record_block) & 0xffffffff)
 
+        assert (len(record_block) == decompressed_size)
+        # split record block according to the offset info from key block
+        #record_start, key_text = self._key_list[i]
+        # reach the end of current record block
+        # record end index
+        record_start=self._key_list[key_index][0]
+        record_end = self._key_list[key_index + 1][0]
+        record = record_block[record_start - block_uncompressed_offset:record_end - block_uncompressed_offset]
+        # convert to utf-8
+        record = record.decode(self._encoding, errors='ignore').strip(u'\x00').encode('utf-8')
+        return self._key_list[key_index][1],record
+
+
+    def _decode_record_block(self):
+
+        f = open(self._fname, 'rb')
+        f.seek(self._record_block_offset
+               + self._number_width*4
+               + self._number_width*2*len(self._record_block_info_list))
 
         # actual record block data
         offset = 0
         i = 0
         size_counter = 0
+        record_block_info_list=self._record_block_info_list
         for compressed_size, decompressed_size in record_block_info_list:
             record_block_compressed = f.read(compressed_size)
             # 4 bytes indicates block compression type
